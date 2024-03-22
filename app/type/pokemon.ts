b2181c4591eab75a2100f07dfc881e18095a343a
MiiJack <f.rama723@gmail.com>
2024-03-22 15:37:18 +0300

    feat: Pokémon type

---
+++ app/type/pokemon.ts
@@ -0,0 +1,22 @@
+interface Pokemon {
+    name: string;
+    url: string;
+}
+
+interface PokemonListProps {
+    pokemons: Pokemon[]
+    page: number
+    totalPages: number
+    fetchPokemons: (page: number) => void
+}
+
+interface PokemonDetails {
+    name: string
+    height: number
+    weight: number
+    types: { type: { name: string } }[]
+    id: number
+    sprites: { front_default: string, front_shiny: string }
+}
+
+export type { Pokemon, PokemonListProps, PokemonDetails }
