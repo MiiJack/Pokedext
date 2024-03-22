7ee7c4700c3f21fe33a4be306000618287d5dd00
MiiJack <f.rama723@gmail.com>
2024-03-22 16:29:03 +0300

    chore: formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import {redirect} from "next/navigation";
 import Link from "next/link";
 
 export default function Home() {
-  return(
-      <div className="mt-4 flex justify-center space-x-4">
-          <Link href={'/client'} className="px-4 py-2 bg-sky-500 text-white rounded-md">Client</Link>
-          <Link href={'/server'} className="px-4 py-2 bg-sky-500 text-white rounded-md">Server</Link>
-      </div>
-  )
+  return (
+    <div className="mt-4 flex justify-center space-x-4">
+      <Link
+        href={"/client"}
+        className="px-4 py-2 bg-sky-500 text-white rounded-md"
+      >
+        Client
+      </Link>
+      <Link
+        href={"/server"}
+        className="px-4 py-2 bg-sky-500 text-white rounded-md"
+      >
+        Server
+      </Link>
+    </div>
+  );
 }
--- app/type/pokemon.ts
+++ app/type/pokemon.ts
@@ -1,22 +1,28 @@
 interface Pokemon {
-    name: string;
-    url: string;
+  name: string;
+  url: string;
 }
 
 interface PokemonListProps {
-    pokemons: Pokemon[]
-    page: number
-    totalPages: number
-    fetchPokemons: (page: number) => void
+  pokemons: Pokemon[];
+  page: number;
+  totalPages: number;
+  fetchPokemons: (page: number) => void;
 }
 
 interface PokemonDetails {
-    name: string
-    height: number
-    weight: number
-    types: { type: { name: string } }[]
-    id: number
-    sprites: { front_default: string, front_shiny: string }
+  name: string;
+  height: number;
+  weight: number;
+  types: { type: { name: string } }[];
+  id: number;
+  sprites: {
+    front_default: string;
+    front_shiny: string;
+    other: {
+      "official-artwork": { front_default: string; front_shiny: string };
+    };
+  };
 }
 
-export type { Pokemon, PokemonListProps, PokemonDetails }
+export type { Pokemon, PokemonListProps, PokemonDetails };
