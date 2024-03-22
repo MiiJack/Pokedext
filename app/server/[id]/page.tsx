3346e0989c8d582b93893abe2ef71da6d8354f04
MiiJack <f.rama723@gmail.com>
2024-03-22 16:28:49 +0300

    chore: changed img tag so it uses Next's

--- app/server/[id]/page.tsx
+++ app/server/[id]/page.tsx
@@ -1,26 +1,44 @@
-import React from 'react'
-import { PokemonDetails } from "../../type/pokemon"
+import React from "react";
+import { PokemonDetails } from "../../type/pokemon";
 import Link from "next/link";
+import Image from "next/image";
 
 const PokemonDetail: React.FC<{ pokemon: PokemonDetails }> = ({ pokemon }) => {
-    const { name, height, weight, types, id, sprites } = pokemon
+  const { name, height, weight, types, id, sprites } = pokemon;
 
-    return (
-        <div className="mx-auto max-w-md p-4">
-            <Link href={`/server/`} className="text-2xl text-transform capitalize font-bold mb-4">{name}</Link>
-            <img src={sprites.front_shiny} alt={name} className="mb-4" />
-            <p className="mb-2">Height: {height / 10}m</p>
-            <p className="mb-2">Weight: {weight / 10}kg</p>
-            <p className="mb-2 text-transform capitalize">Types: {types.map(({ type }) => type.name).join(', ')}</p>
-            <p className="mb-2">National Pokedex Number: {id}</p>
-        </div>
-    )
-}
+  return (
+    <div className="mx-auto max-w-md p-4">
+      <Link
+        href={`/server/`}
+        className="text-2xl text-transform capitalize font-bold mb-4"
+      >
+        {name}
+      </Link>
+      <Image
+        width={400}
+        height={400}
+        src={sprites.other["official-artwork"].front_shiny}
+        alt={name}
+        className="mb-4"
+      />
+      <p className="mb-2">Height: {height / 10}m</p>
+      <p className="mb-2">Weight: {weight / 10}kg</p>
+      <p className="mb-2 text-transform capitalize">
+        Types: {types.map(({ type }) => type.name).join(", ")}
+      </p>
+      <p className="mb-2">National Pokedex Number: {id}</p>
+    </div>
+  );
+};
 
-export default async function PokemonPage({ params }: { params: { id: string } }) {
-    const id = params.id
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    const pokemon: PokemonDetails = await res.json()
+export default async function PokemonPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const id = params.id;
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const pokemon: PokemonDetails = await res.json();
 
-    return pokemon ? <PokemonDetail pokemon={pokemon} /> : <div>Loading...</div>
+  return pokemon ? <PokemonDetail pokemon={pokemon} /> : <div>Loading...</div>;
 }
--- app/server/page.tsx
+++ app/server/page.tsx
@@ -1,73 +1,98 @@
-import React from 'react'
-import Link from 'next/link'
-import { Pokemon, PokemonListProps } from '../type/pokemon'
+import React from "react";
+import Link from "next/link";
+import { Pokemon, PokemonListProps } from "../type/pokemon";
+import Image from "next/image";
 
-const PokemonList: React.FC<PokemonListProps> = ({ pokemons, page, totalPages, fetchPokemons }) => {
-    return (
-        <div className="container mx-auto px-4 py-8">
-            <Link href={`/`} className="text-3xl font-bold mb-6">Pokemon List (Server)</Link>
-            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {pokemons.map(({ name, url }, index) => (
-                    <li key={index} className="bg-slate-800  rounded-lg shadow-md overflow-hidden">
-                        <div className="p-4 backdrop-blur-sm">
-                            <span className="text-lg text-rose-600 text-transform capitalize font-semibold mb-2">{name}</span>
-                            <img
-                                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${url.split('/').slice(-2, -1)}.png`}
-                                alt={name}
-                                className="w-full h-auto"
-                            />
-                            <Link href={`/server/${url.split('/').slice(-2, -1)}`} passHref>
-                                <button className="px-4 py-2 bg-blue-500 text-white rounded-md mt-2">Details</button>
-                            </Link>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-            <div className="mt-6 text-center">
-                Page {page} of {totalPages}
+const PokemonList: React.FC<PokemonListProps> = ({
+  pokemons,
+  page,
+  totalPages,
+  fetchPokemons,
+}) => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <Link href={`/`} className="text-3xl font-bold mb-6">
+        Pokemon List (Server)
+      </Link>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {pokemons.map(({ name, url }, index) => (
+          <li
+            key={index}
+            className="bg-slate-800  rounded-lg shadow-md overflow-hidden"
+          >
+            <div className="p-4 backdrop-blur-sm">
+              <span className="text-lg text-rose-600 text-transform capitalize font-semibold mb-2">
+                {name}
+              </span>
+              <Image
+                width={192}
+                height={192}
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${url.split("/").slice(-2, -1)}.png`}
+                alt={name}
+                className="w-full h-auto"
+              />
+              <Link href={`/server/${url.split("/").slice(-2, -1)}`} passHref>
+                <button className="px-4 py-2 bg-blue-500 text-white rounded-md mt-2">
+                  Details
+                </button>
+              </Link>
             </div>
-            <div className="mt-4 flex justify-center space-x-4">
-                {page > 1 && (
-                    <Link href={`/server?page=${page - 1}`} passHref>
-                        <button className="px-4 py-2 bg-blue-500 text-white rounded-md">Previous</button>
-                    </Link>
-                )}
-                {page < totalPages && (
-                    <Link href={`/server?page=${page + 1}`} passHref>
-                        <button className="px-4 py-2 bg-blue-500 text-white rounded-md">Next</button>
-                    </Link>
-                )}
-            </div>
-        </div>
-    )
-}
+          </li>
+        ))}
+      </ul>
+      <div className="mt-6 text-center">
+        Page {page} of {totalPages}
+      </div>
+      <div className="mt-4 flex justify-center space-x-4">
+        {page > 1 && (
+          <Link href={`/server?page=${page - 1}`} passHref>
+            <button className="px-4 py-2 bg-blue-500 text-white rounded-md">
+              Previous
+            </button>
+          </Link>
+        )}
+        {page < totalPages && (
+          <Link href={`/server?page=${page + 1}`} passHref>
+            <button className="px-4 py-2 bg-blue-500 text-white rounded-md">
+              Next
+            </button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
 
 const fetchPokemons = async (page: number) => {
-    const offset = (page - 1) * 50
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${offset}`)
-    const data = await res.json()
+  const offset = (page - 1) * 50;
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=50&offset=${offset}`,
+  );
+  const data = await res.json();
 
-    const pokemons: Pokemon[] = data.results
-    const totalPages = Math.ceil(data.count / 50)
+  const pokemons: Pokemon[] = data.results;
+  const totalPages = Math.ceil(data.count / 50);
 
-    return { pokemons, page, totalPages }
-}
+  return { pokemons, page, totalPages };
+};
 
-export default async function PokemonPage(props : any) {
-    const page = props.searchParams.page ? parseInt(props.searchParams.page) : 1;
-    const offset = (page - 1) * 50;
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${offset}`);
-    const data = await res.json();
+export default async function PokemonPage(props: any) {
+  const page = props.searchParams.page ? parseInt(props.searchParams.page) : 1;
+  const offset = (page - 1) * 50;
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=50&offset=${offset}`,
+  );
+  const data = await res.json();
 
-    const pokemons: Pokemon[] = data.results;
-    const totalPages = Math.ceil(data.count / 50);
+  const pokemons: Pokemon[] = data.results;
+  const totalPages = Math.ceil(data.count / 50);
 
-    return (
-        <PokemonList
-            pokemons={pokemons}
-            page={page}
-            totalPages={totalPages}
-            fetchPokemons={fetchPokemons}
-        />
-    )
+  return (
+    <PokemonList
+      pokemons={pokemons}
+      page={page}
+      totalPages={totalPages}
+      fetchPokemons={fetchPokemons}
+    />
+  );
 }
